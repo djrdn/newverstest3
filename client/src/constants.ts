9da526d0e9664889fa479a49fe03e8d93273764e
shef <shef@DESKTOP-ILLG190>
2024-05-07 20:00:02 +0300

    Add files for deploy on docker

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'none' : 'http://localhost:3000';
-//export const BASE_URL = process.env.NODE_ENV === 'production' ? 'none' : 'https://serverphobl-f11e26a27b4b.herokuapp.com/';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'http://localhost:3000';
 
 
